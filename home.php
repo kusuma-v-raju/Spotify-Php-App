eace14adc87eb6cca230e5ac67988936f657102b
kusuma <kusumaraju777@gmail.com>
2022-08-03 14:03:40 +0200

    Home page

---
+++ home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    include_once 'menu.html'
+    ?>
+    
+    <h1>Spotify</h1>
+    <p>Spotify offers digital copyright restricted recorded music and podcasts, including more than 82 million songs, from record labels and media companies.[8] As a freemium service, basic features are free with advertisements and limited control, while additional features, such as offline listening and commercial-free listening, are offered via paid subscriptions. Spotify is currently available in 180+ countries, as of October 2021. Users can search for music based on artist, album, or genre, and can create, edit, and share playlists. <br>
+
+
+    <br>Spotify is available in most of Europe, as well as the Americas and Oceania, with a total availability in 184 markets.[9] The service is available on most devices including Windows, macOS, and Linux computers, iOS and Android smartphones and tablets, smart home devices such as the Amazon Echo and Google Nest lines of products and digital media players like Roku.[10] <br>
+
+
+    <br>Unlike physical or download sales, which pay artists a fixed price per song or album sold, Spotify pays royalties based on the number of artist streams as a proportion of total songs streamed. It distributes approximately 70% of its total revenue to rights holders (often record labels), who then pay artists based on individual agreements.[11] According to Ben Sisario of The New York Times, approximately 13,000 out of seven million artists on Spotify generated $50,000 or more in payments in 2020.[12]</p>
+</body>
+</html>
